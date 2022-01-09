3608a6d4ca644b77604e6dea3219283225d34c15
ilantimerman <ilan.tmrmn@gmail.com>
2022-01-09 12:06:18 -0700

    defining variables, adding prompts and confirmations

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,31 @@
 //Assignment code here
+let generateBtn = document.querySelector("#generate");
 
+//prompts and confirmations
+let length = prompt("How many characters would you like to use between 8 and 128?");
+let uppercaseuse = confirm("Would you like to use upper-case characters?");
+let lowercaseuse = confirm("Would you like to use lower-case characters?");
+let numberuse = confirm("Would you like to use numbers?");
+let specialuse = confirm("Would you like to use special characters?");
+let password = "";
+
+//define character sets
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var numbers = "1234567890";
+var special = "$&*-?@~";
+
+//define generatePassword function
 function generatePassword () {
-  var length=10,
-      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890$&*-?@~",
+  var length = 10,
+
       password = "";
   for (var i = 0; i < length; i++) {
     password += characters.charAt (Math.floor(Math.random() * charset.length));
   }
   return password;
 }
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");

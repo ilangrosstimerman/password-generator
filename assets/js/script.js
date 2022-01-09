a036d9df9881680dc1c7705fef93dad38c360df6
ilantimerman <ilan.tmrmn@gmail.com>
2022-01-09 12:39:11 -0700

    introduce conditional in case length is outside parameters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,27 @@
 //Assignment code here
-let generateBtn = document.querySelector("#generate");
-
-//prompts and confirmations
-let length = prompt("How many characters would you like to use between 8 and 128?");
-let uppercaseuse = confirm("Would you like to use upper-case characters?");
-let lowercaseuse = confirm("Would you like to use lower-case characters?");
-let numberuse = confirm("Would you like to use numbers?");
-let specialuse = confirm("Would you like to use special characters?");
-let password = "";
 
 //define character sets
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "1234567890";
 var special = "$&*-?@~";
+
+//conditionals and confirmations
+let length = prompt("How many characters would you like to use between 8 and 128?");
+
+if (length < 8 || length > 128) {
+  alert("Password must be between 8 and 128 characters");
+  location.reload()
+  }
+
+else{
+let uppercaseuse = confirm("Would you like to use upper-case characters?");
+let lowercaseuse = confirm("Would you like to use lower-case characters?");
+let numberuse = confirm("Would you like to use numbers?");
+let specialuse = confirm("Would you like to use special characters?");
+let initialvalue = "";
+}
+
 
 //define generatePassword function
 function generatePassword () {

4c6941c10670caab70704ab2694885f3f562b29b
ilantimerman <ilan.tmrmn@gmail.com>
2022-01-09 11:22:25 -0700

    define generatePassword function, not yet working

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,20 @@
-// Assignment code here
+//Assignment code here
 
+function generatePassword () {
+  var length=10,
+      charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890$&*-?@~",
+      password = "";
+  for (var i = 0; i < length; i++) {
+    password += characters.charAt (Math.floor(Math.random() * charset.length));
+  }
+  return password;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
+
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");

34ce7980330601dba73d3df62633f7135e0ddc54
ilantimerman <ilan.tmrmn@gmail.com>
2022-01-09 18:16:36 -0700

    working code for password generation, however if statements don't work as expected

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,66 +1,68 @@
-//Assignment code here --
-
 //define character sets
+
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var numbers = "1234567890";
 var special = "$&*-?@~";
 
-//conditionals and confirmations
-let length = prompt("How many characters would you like to use between 8 and 128?");
+//define initial variables
 
-if (length < 8 || length > 128) {
-  alert("Password must be between 8 and 128 characters");
-  location.reload()
-  }
+var characters = "";
+var length= 8;
+var generateBtn = document.querySelector("#generate");
 
-else{
-uppercaseuse = confirm("Would you like to use upper-case characters?");
-lowercaseuse = confirm("Would you like to use lower-case characters?");
-numberuse = confirm("Would you like to use numbers?");
-specialuse = confirm("Would you like to use special characters?");
-password = "";
-}
+//set password generation function
 
-//add values depending on confirms
-
-if (uppercaseuse) {
-password += uppercase
-}
-
-if (lowercaseuse) {
-  password += lowercase
-}
-
-if (numberuse) {
-  password += numbers
-}
-
-if (specialuse) {
-  password += special
-}
-
-//define generatePassword function
-function generatePassword () {
-  var length = 10,
-
-      password = "";
-  for (var i = 0; i < length; i++) {
-    password += characters.charAt (Math.floor(Math.random() * charset.length));
+function generatePassword(length, characters) {
+  var password = "";
+  for (let i = 0; i < length; i++) {
+    password += characters.charAt(Math.floor(Math.random() * characters.length));
   }
   return password;
 }
 
-
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
+// write password to the #password input
 
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+var password = "";
+var passwordText = document.querySelector("#password");
 
+//make length = to the number specified in the prompt
+
+length = window.prompt("How many characters would you like to use between 8 and 128?");
+
+//set parameters based on prompt and confirms
+
+if ((length >= 8) && (length <= 128)) {
+uppercaseuse = window.confirm("Would you like to use upper-case characters?");
+  if (uppercaseuse) {
+   characters += uppercase;
+    }
+lowercaseuse = window.confirm("Would you like to use lower-case characters?");
+  if (lowercaseuse) {
+   characters += lowercase;
+    }
+numberuse = window.confirm("Would you like to use numbers?");
+  if (numberuse) {
+    characters += numbers;
+    }
+specialuse = window.confirm("Would you like to use special characters?");
+  if (specialuse) {
+    characters += special;
+    }
+} 
+
+//set action if number isn't within length parameters needed
+
+else {
+  alert("Password must be between 8 and 128 characters");
+  location.reload()
+}
+
+//specify running conditions for function generatePassword
+//set passwordText value
+
+  password = generatePassword(length, characters);
   passwordText.value = password;
 
 }
